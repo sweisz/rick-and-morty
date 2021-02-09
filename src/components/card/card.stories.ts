a21b8ff106e2554a269ed14fecae8da2080900cc
sweisz <dev@steffenweisz.de>
2021-02-09 13:54:52 +0100

    Add Multiple Cards Layout.

--- src/components/card/card.stories.ts
+++ src/components/card/card.stories.ts
@@ -1,5 +1,6 @@
 import "./card.css";
 import { createCard } from "./card";
+import { createElement } from "../../utils/createElement";
 
 export default {
   title: "Components/Card",
@@ -23,3 +24,31 @@ export const Morty = () =>
     species: "Human",
     origin: "Earth (C-137)",
   });
+
+export const Multiple = () => {
+  const characters = [
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      name: "Rick Sanchenz",
+      status: "Alive",
+      species: "Human",
+      origin: { name: "Earth (C-137)" },
+    },
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      name: "Morty Smith",
+      status: "Dead",
+      species: "Human",
+      origin: { name: "Earth (C-137)" },
+    },
+  ];
+  // todo: display multiple characters based on `characters`.
+  // you can use `createElement` here to create a container.
+  // don't forget to return the container.
+
+  const container = createElement("div", {
+    className: "container",
+    childs: characters.map((characters) => createCard(characters)),
+  });
+  return container;
+};
