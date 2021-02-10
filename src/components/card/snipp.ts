575b2523197aa6e64cc9e2a358f4d9357702565f
sweisz <dev@steffenweisz.de>
2021-02-10 22:06:31 +0100

    Removed temp file

--- src/components/card/snipp.ts
+++
@@ -1,8 +0,0 @@
-const random = Math.round(Math.random() * 6670 + 1);
-console.log({ random });
-
-const character = await getCharacter(random);
-console.log({ character });
-
-const randomCharacterCard = createCard(character);
-container.append(randomCharacterCard);
